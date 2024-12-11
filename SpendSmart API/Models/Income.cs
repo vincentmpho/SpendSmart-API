4429ca9c6bd3922ad99e57fdd3b1fbb989b043bb
MphoM <MphoM@MP25WBGZ-NB>
2024-12-11 22:04:32 +0200

    create models for UserProfile, Income, Expense, and TodoTransaction

---
+++ SpendSmart API/Models/Income.cs
@@ -0,0 +1,9 @@
+﻿namespace SpendSmart_API.Models
+{
+    public class Income
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ SpendSmart API/Models/TodoTransaction.cs
@@ -0,0 +1,9 @@
+﻿namespace SpendSmart_API.Models
+{
+    public class TodoTransaction
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
