2ccb9ee70c2f40520b8cf296727af15ad86eaade
MphoM <MphoM@MP25WBGZ-NB>
2024-12-11 22:41:32 +0200

    configure CORS and DbContext for SpendSmart API - Added CORS policy to allow all origins, methods, and headers. - Configured `ApplicationDbContext` with SQL Server connection string. - Integrated middleware for CORS and HTTPS redirection. - Updated `Program.cs` for streamlined service registration.

---
+++ SpendSmart API/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendSmart_API.Models;
+
+namespace SpendSmart_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+     
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<TodoTransaction> TodoTransactions { get; set; }
+    }
+}
