f2c7b5be0606e49bfc0222e1c38d092720d23f38
MphoM <MphoM@MP25WBGZ-NB>
2024-12-13 14:56:14 +0200

    Implemented the ExpenseEndpoint and updated both the migrations and the Expenses model class.

--- SpendSmart API/Models/Expense.cs
+++ SpendSmart API/Models/Expense.cs
@@ -3,8 +3,9 @@
     public class Expense
     {
         public int Id { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
-        public string Description { get; set; }
+        public string Month { get; set; }
+        public string ExpenseType { get; set; } 
+        public decimal ExpenseAmount { get; set; }
     }
+
 }
