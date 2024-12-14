36902e31506dde0bf79baf9183d2ac9af8528815
MphoM <MphoM@MP25WBGZ-NB>
2024-12-14 20:07:23 +0200

    Implement The ProfileEndpoint

---
+++ SpendSmart API/Controllers/ProfileController .cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SpendSmart_API.Data;
+using SpendSmart_API.Models;
+
+namespace SpendSmart_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProfile([FromBody] Profile profile)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Profiles.Add(profile);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProfileById), new { id = profile.Id }, profile);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProfileById(int id)
+        {
+            var profile = await _context.Profiles.FindAsync(id);
+
+            if (profile == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(profile);
+        }
+    }
+}
